45d5a6c701159eac18a68a7a5ee60ed208d7ac12
Dondecoder <patrikfougere@gmail.com>
2022-04-29 21:07:23 -0400

    Updated main.py to include comments

--- Remove Duplicates from Linked List/main.py
+++ Remove Duplicates from Linked List/main.py
@@ -1,3 +1,10 @@
+# This is an input class. Do not edit.
+# write a function that returns a modified version that includes no duplicates
+# test cases = 3-->4--->5--->6--->6----->6--->7-->7 return 3-->4--->5--->6--->7
+# first we would need to iterate through linkedlist
+# keep track of values in the linked list using a count or something
+# if count == itr then itr = self.next
+
 class LinkedList:
     def __init__(self, value):
         self.value = value
